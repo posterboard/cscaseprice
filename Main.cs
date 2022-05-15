464c684b7773ad228fd42559f25551c868f99436
posterboard <67428806+posterboard@users.noreply.github.com>
2022-05-15 09:36:04 -0400

    inclduesa all cases now

--- Main.cs
+++ Main.cs
@@ -3,57 +3,88 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.IO;
-
+using System.Collections.Generic;
 class Program
 {
     public static void Main(string[] args)
     {
-        int currency = 3;//
+        String direct =  Environment.GetLogicalDrives()[0]+"/Users/"+ Environment.UserName+"/Desktop/GloveOutput.txt";
+  
+        updateCaseValues(1, direct);
+
+    }
+    private static void updateCaseValues(int c, string directory)
+    {
+        int currency = c;//
 
 
 
-        //  Task<string> a = client.GetStringAsync("" + currency + "" + urlExtension);
-        string outputDir =Environment.GetLogicalDrives()[0]+"/Users/"+ Environment.UserName+"/Desktop/GloveOutput.txt";
-        StreamWriter sw = new StreamWriter(outputDir);
-        /*
-        Csgo Weapn Case, Operation Bravo Case, CSGO weapon case 2, winter offensive casee, csgo weapon case3,
-         operatiotn phoniex, huntsman, operation breakout, operation vanguard, chroma, chroma 2, falchion case, shadow case, revolver case,
-        operation wildfire, chroma 3, gamma, gamma 2, glove, spectrum,  operation hydra, spectrum 2,clutch,  horizon case, danger zone, primsa, cs20,
-        prisma 2, fracture, snakebite, dreams and nightmares
-         
-         */
-        string[] caseNames = new string[] { "CSGO Weapon Case","Operation Bravo Case","CSGO weapon case 2","Winter Offensive Case"," CSGO Weapon Case 3","Operation Phoneix Case","Huntsman Case","Operation Breakout Case"," Operation Vanguard Case", "Chroma Case", "Chroma 2 Case "," Falchion case"," Shadow Case"," Revolver Case","Operation Wildfire Case","Chroma 3 Case","Gamma Case", "Gamma 2 Case","Glove Case","Spectrum Case","Operation Hydra Case", "Spectrum 2 Case","Clutch Case","Horizon Case","Danger Zone Case","Prisma Case","CS20 Case","Operation Shattered Web Case","Primsa 2 Case","Fracture Case","Operation Broken Fang Case","Snakebite Case","Operation Riptide Case","Dreams & Nightmares Case"};
-        string[] urlExtensionList = new string[] { "CS:GO%20Weapon%20Case", "Operation%20Bravo%20Case","CS%3AGO%20Weapon%20Case%202", "Winter%20Offensive%20Weapon%20Case", "CS%3AGO%20Weapon%20Case%203" , "Operation%20Phoenix%20Weapon%20Case", "Huntsman%20Weapon%20Case", "Operation%20Breakout%20Weapon%20Case", "Operation%20Vanguard%20Weapon%20Case", "Chroma%20Case" , "Chroma%202%20Case", "Falchion%20Case" , "Shadow%20Case","Revolver%20Case", "Operation%20Wildfire%20Case", "Chroma%203%20Case", "Gamma%20Case", "Gamma%202%20Case", "Glove%20Case", "Spectrum%20Case", "Operation%20Hydra%20Case", "Spectrum%202%20Case", "Clutch%20Case", "Horizon%20Case", "Danger%20Zone%20Case", "Prisma%20Case" };
-        string[] output = fetchApi(urlExtensionList, 3);
+        string outputDir = directory;
+        StreamWriter sw = new StreamWriter(outputDir, append: true);
+        string[] caseNames = new string[] { "CSGO Weapon Case", "Operation Bravo Case", "CSGO weapon case 2", "Winter Offensive Case", " CSGO Weapon Case 3", "Operation Phoneix Case", "Huntsman Case", "Operation Breakout Case", " Operation Vanguard Case", "Chroma Case", "Chroma 2 Case ", " Falchion case", " Shadow Case", " Revolver Case", "Operation Wildfire Case", "Chroma 3 Case", "Gamma Case", "Gamma 2 Case", "Glove Case", "Spectrum Case", "Operation Hydra Case", "Spectrum 2 Case", "Clutch Case", "Horizon Case", "Danger Zone Case", "Prisma Case", "CS20 Case", "Operation Shattered Web Case", "Primsa 2 Case", "Fracture Case", "Operation Broken Fang Case", "Snakebite Case", "Operation Riptide Case", "Dreams & Nightmares Case" };
+        string[] urlExtensionList = new string[] { "CS:GO%20Weapon%20Case", "Operation%20Bravo%20Case", "CS%3AGO%20Weapon%20Case%202", "Winter%20Offensive%20Weapon%20Case", "CS%3AGO%20Weapon%20Case%203", "Operation%20Phoenix%20Weapon%20Case", "Huntsman%20Weapon%20Case", "Operation%20Breakout%20Weapon%20Case", "Operation%20Vanguard%20Weapon%20Case", "Chroma%20Case", "Chroma%202%20Case", "Falchion%20Case", "Shadow%20Case", "Revolver%20Case", "Operation%20Wildfire%20Case", "Chroma%203%20Case", "Gamma%20Case", "Gamma%202%20Case", "Glove%20Case", "Spectrum%20Case", "Operation%20Hydra%20Case", "Spectrum%202%20Case", "Clutch%20Case", "Horizon%20Case", "Danger%20Zone%20Case", "Prisma%20Case", "CS20%20Case" , "Shattered%20Web%20Case", "Prisma%202%20Case", "Fracture%20Case", "Operation%20Broken%20Fang%20Case", "Snakebite%20Case", "Operation%20Riptide%20Case", "Dreams%20%26%20Nightmares%20Case" };
+        string[] output = new string[urlExtensionList.Length]; //fetchApi(urlExtensionList, currency,client);
 
-        for (int i = 0; i < output.Length; i++)
+        sw.WriteLine(DateTime.Now);
+        HttpClient client = new HttpClient();
+        for (int i = 0; i < urlExtensionList.Length; i++)
         {
 
-            sw.Write(output[i]);
-            sw.WriteLine();
+            sw.WriteLine(fetchApi(urlExtensionList[i], 1, client));
+
         }
         sw.Close();
 
-
-
+        Console.WriteLine("Not here");
 
     }
-    private static string[] fetchApi(string[] urlExtensionArray, int currency)
+    private static string fetchApi(string urlExtension, int currency, HttpClient c)
     {
-        HttpClient client = new HttpClient();
-        string urlExtension = "https://steamcommunity.com/market/priceoverview/?appid=730&currency=" + currency + "&market_hash_name=";
+        HttpClient client = c;
+        string url = "https://steamcommunity.com/market/priceoverview/?appid=730&currency=" + currency + "&market_hash_name=";
         string[] jsonString = new string[urlExtension.Length];
         Task<string> temp;
-        Console.WriteLine(urlExtensionArray.Length);
-        for (int i = 0; i < urlExtensionArray.Length; i++)
+        bool pass = false;
+        bool reset = false;
+        string output = "";
+        while (!pass)
         {
-            Console.WriteLine(i);
-            temp = client.GetStringAsync(urlExtension + urlExtensionArray[i]);
-            Thread.Sleep(3000);
-            jsonString[i] = temp.Result;
-        } 
+            reset = true;
+            pass = true;
+            try
+            {
+                temp = client.GetStringAsync(url + urlExtension);
+                //Thread.Sleep(1000);
+                output = temp.Result;
+                Console.WriteLine("Sucess " + urlExtension);
+            }
+            catch (Exception e)
+            {
+                if (reset)
+                {
 
-        return jsonString;
+                    reset = false;
 
+                }
+                pass = false;
+                Console.WriteLine("Failed"+urlExtension);
+            }
+
+            //my only solution to Error 429
+            if (!pass)
+            {
+                Thread.Sleep(3000);
+
+            }
+
+        }
+        var tempList = new List<string>();
+        foreach (var s in jsonString)
+        {
+            if (!string.IsNullOrEmpty(s))
+                tempList.Add(s);
+        }
+        jsonString = tempList.ToArray();
+        return output;
     }
 }

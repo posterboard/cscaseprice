8fd925e8f754dadf693753a5549e2d588038433a
posterboard <67428806+posterboard@users.noreply.github.com>
2022-05-19 19:58:28 -0400

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -11,8 +11,11 @@ class Program
     public static void Main(string[] args)
     {
         String direct =  Environment.GetLogicalDrives()[0]+"/Users/"+ Environment.UserName+"/Desktop/CaseOutput.txt";
-  
-        updateCaseValues(1, direct);
+        var startTimeSpan = TimeSpan.Zero;
+        var periodTimeSpan = TimeSpan.FromHours(1);
+
+       
+        updateCaseValues(1, direct);      
         
     }
     private static void updateCaseValues(int c, string directory)
@@ -27,7 +30,7 @@ class Program
         string[] urlExtensionList = new string[] { "CS:GO%20Weapon%20Case", "Operation%20Bravo%20Case", "CS%3AGO%20Weapon%20Case%202", "Winter%20Offensive%20Weapon%20Case", "CS%3AGO%20Weapon%20Case%203", "Operation%20Phoenix%20Weapon%20Case", "Huntsman%20Weapon%20Case", "Operation%20Breakout%20Weapon%20Case", "Operation%20Vanguard%20Weapon%20Case", "Chroma%20Case", "Chroma%202%20Case", "Falchion%20Case", "Shadow%20Case", "Revolver%20Case", "Operation%20Wildfire%20Case", "Chroma%203%20Case", "Gamma%20Case", "Gamma%202%20Case", "Glove%20Case", "Spectrum%20Case", "Operation%20Hydra%20Case", "Spectrum%202%20Case", "Clutch%20Case", "Horizon%20Case", "Danger%20Zone%20Case", "Prisma%20Case" };
         string[] output = new string[urlExtensionList.Length]; //fetchApi(urlExtensionList, currency,client);
 
-        sw.WriteLine(DateTime.Now);
+        
         HttpClient client = new HttpClient();
         string result;
         CaseData currentCase;
@@ -40,6 +43,7 @@ class Program
             //insert name into json
             currentCase = deSerializeJson(result);
             currentCase.name=caseNames[i];
+            currentCase.dateTime=DateTime.Now.ToString();
 
             sw.WriteLine(JsonSerializer.Serialize(currentCase));
         }
@@ -122,6 +126,7 @@ class Program
     public class CaseData
     {
         public string name { get; set; }
+        public string dateTime { get; set; }
         public string lowest_price { get; set; }
         public string volume { get; set; }
         public string median_price { get; set; }

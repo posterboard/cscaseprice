eacc8da18e8b6ba5803f0a23d17df9d557e9a4ee
posterboard <67428806+posterboard@users.noreply.github.com>
2022-05-23 20:35:16 -0400

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -11,33 +11,34 @@ using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.DynamoDBv2;
 class Program
 {
-    private static AmazonDynamoDBClient aClient = new AmazonDynamoDBClient(RegionEndpoint.USEast1);
+    
 
     public static void Main(string[] args)
     {
-        writeData();
-        String direct =  Environment.GetLogicalDrives()[0]+"/Users/"+ Environment.UserName+"/Desktop/CaseOutput.txt";
-  
-        updateCaseValues(1, direct);
+        //String direct =  Environment.GetLogicalDrives()[0]+"/Users/"+ Environment.UserName+"/Desktop/CaseOutput.txt";
+        String direct = "./CaseOutput.txt";//all recorded case prices
+        String direct2 = "./tmpCaseOutput.txt";//most recent case prices
+        updateCaseValues(1, direct,direct2);
         
     }
-    private static void updateCaseValues(int c, string directory)
+    private static void updateCaseValues(int c, string directory,string directory2)
     {
         int currency = c;//
 
 
 
-        string outputDir = directory;
-        StreamWriter sw = new StreamWriter(outputDir, append: true);
+       
+        StreamWriter sw = new StreamWriter(directory, append: true);
+        StreamWriter sw2 = new StreamWriter(directory2);
         string[] caseNames = new string[] { "CSGO Weapon Case", "Operation Bravo Case", "CSGO weapon case 2", "Winter Offensive Case", " CSGO Weapon Case 3", "Operation Phoneix Case", "Huntsman Case", "Operation Breakout Case", " Operation Vanguard Case", "Chroma Case", "Chroma 2 Case ", " Falchion case", " Shadow Case", " Revolver Case", "Operation Wildfire Case", "Chroma 3 Case", "Gamma Case", "Gamma 2 Case", "Glove Case", "Spectrum Case", "Operation Hydra Case", "Spectrum 2 Case", "Clutch Case", "Horizon Case", "Danger Zone Case", "Prisma Case", "CS20 Case", "Operation Shattered Web Case", "Primsa 2 Case", "Fracture Case", "Operation Broken Fang Case", "Snakebite Case", "Operation Riptide Case", "Dreams & Nightmares Case" };
         string[] urlExtensionList = new string[] { "CS:GO%20Weapon%20Case", "Operation%20Bravo%20Case", "CS%3AGO%20Weapon%20Case%202", "Winter%20Offensive%20Weapon%20Case", "CS%3AGO%20Weapon%20Case%203", "Operation%20Phoenix%20Weapon%20Case", "Huntsman%20Weapon%20Case", "Operation%20Breakout%20Weapon%20Case", "Operation%20Vanguard%20Weapon%20Case", "Chroma%20Case", "Chroma%202%20Case", "Falchion%20Case", "Shadow%20Case", "Revolver%20Case", "Operation%20Wildfire%20Case", "Chroma%203%20Case", "Gamma%20Case", "Gamma%202%20Case", "Glove%20Case", "Spectrum%20Case", "Operation%20Hydra%20Case", "Spectrum%202%20Case", "Clutch%20Case", "Horizon%20Case", "Danger%20Zone%20Case", "Prisma%20Case" };
         string[] output = new string[urlExtensionList.Length]; //fetchApi(urlExtensionList, currency,client);
 
-        sw.WriteLine(DateTime.Now);
+
         HttpClient client = new HttpClient();
         string result;
         CaseData currentCase;
-        var options = new JsonSerializerOptions{WriteIndented = true};
+        var options = new JsonSerializerOptions { WriteIndented = true };
         for (int i = 0; i < urlExtensionList.Length; i++)
         {
             result = fetchApi(urlExtensionList[i], 1, client);
@@ -45,20 +46,21 @@ class Program
 
             //insert name into json
             currentCase = deSerializeJson(result);
-            currentCase.name=caseNames[i];
+            currentCase.name = caseNames[i];
+            currentCase.dateTime = DateTime.Now.ToString();
 
             sw.WriteLine(JsonSerializer.Serialize(currentCase));
+            sw2.WriteLine(JsonSerializer.Serialize(currentCase));
         }
         sw.Close();
-
+        sw2.Close();
         Console.WriteLine("Not here");
-        
+
     }
     private static string fetchApi(string urlExtension, int currency, HttpClient c)
     {
         HttpClient client = c;
         string url = "https://steamcommunity.com/market/priceoverview/?appid=730&currency=" + currency + "&market_hash_name=";
-        string[] jsonString = new string[urlExtension.Length];
         Task<string> temp;
         bool pass = false;
         bool reset = false;
@@ -95,37 +97,12 @@ class Program
 
         }
         var tempList = new List<string>();
-        foreach (var s in jsonString)
-        {
-            if (!string.IsNullOrEmpty(s))
-                tempList.Add(s);
-        }
-        jsonString = tempList.ToArray();
+
         return output;
     }
-    private static string[] deSerializeJsonFromFile(String fileLoc)
-    {
-        StreamReader r = new StreamReader(fileLoc);
-        string json = r.ReadToEnd();
-        Console.WriteLine(json);
-        string[] h = new string[2];
-        return h;
-    }
+   
 
-    private static void writeData()
-    {
-        
-       
-        try
-        {
-            Table t = Table.LoadTable(aClient,"Cases");
-
-        }catch(Exception e)
-        {
-            Console.WriteLine(e.ToString());
-        }
-       
-    }
+    
     private static CaseData deSerializeJson(String json)
     {
         //%20 =spacae
@@ -140,9 +117,10 @@ class Program
     public class CaseData
     {
         public string name { get; set; }
+        public string dateTime { get; set; }
         public string lowest_price { get; set; }
         public string volume { get; set; }
         public string median_price { get; set; }
-        
+
     }
 }

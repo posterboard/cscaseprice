2b062ea40ac94436dae5cf597d7ade15ed0bd776
posterboard <67428806+posterboard@users.noreply.github.com>
2022-06-19 20:51:00 -0400

    Changed output file path to be in appdata

--- Main.cs
+++ Main.cs
@@ -6,32 +6,36 @@ using System.IO;
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using Amazon;
-using Amazon.DynamoDBv2.DocumentModel;
-using Amazon.DynamoDBv2;
+//updated 6/18/22
 class Program
 {
-    
+
 
     public static void Main(string[] args)
     {
         //String direct =  Environment.GetLogicalDrives()[0]+"/Users/"+ Environment.UserName+"/Desktop/CaseOutput.txt";
-        String direct = "./CaseOutput.txt";//all recorded case prices
-        String direct2 = "./tmpCaseOutput.txt";//most recent case prices
-        updateCaseValues(1, direct,direct2);
-        
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)+"/scrappy";
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+            Console.WriteLine(folderPath);
+        }
+        String direct = Path.Combine(folderPath, "CaseOutput.txt") ;//all recorded case prices
+        String direct2 = Path.Combine(folderPath,"tmpCaseOutput.txt");//most recent case prices 
+        updateCaseValues(1, direct, direct2);
+        Console.WriteLine(direct);
     }
-    private static void updateCaseValues(int c, string directory,string directory2)
+    private static void updateCaseValues(int c, string directory, string directory2)
     {
         int currency = c;//
 
 
 
-       
+
         StreamWriter sw = new StreamWriter(directory, append: true);
         StreamWriter sw2 = new StreamWriter(directory2);
-        string[] caseNames = new string[] { "CSGO Weapon Case", "Operation Bravo Case", "CSGO weapon case 2", "Winter Offensive Case", " CSGO Weapon Case 3", "Operation Phoneix Case", "Huntsman Case", "Operation Breakout Case", " Operation Vanguard Case", "Chroma Case", "Chroma 2 Case ", " Falchion case", " Shadow Case", " Revolver Case", "Operation Wildfire Case", "Chroma 3 Case", "Gamma Case", "Gamma 2 Case", "Glove Case", "Spectrum Case", "Operation Hydra Case", "Spectrum 2 Case", "Clutch Case", "Horizon Case", "Danger Zone Case", "Prisma Case", "CS20 Case", "Operation Shattered Web Case", "Primsa 2 Case", "Fracture Case", "Operation Broken Fang Case", "Snakebite Case", "Operation Riptide Case", "Dreams & Nightmares Case" };
-        string[] urlExtensionList = new string[] { "CS:GO%20Weapon%20Case", "Operation%20Bravo%20Case", "CS%3AGO%20Weapon%20Case%202", "Winter%20Offensive%20Weapon%20Case", "CS%3AGO%20Weapon%20Case%203", "Operation%20Phoenix%20Weapon%20Case", "Huntsman%20Weapon%20Case", "Operation%20Breakout%20Weapon%20Case", "Operation%20Vanguard%20Weapon%20Case", "Chroma%20Case", "Chroma%202%20Case", "Falchion%20Case", "Shadow%20Case", "Revolver%20Case", "Operation%20Wildfire%20Case", "Chroma%203%20Case", "Gamma%20Case", "Gamma%202%20Case", "Glove%20Case", "Spectrum%20Case", "Operation%20Hydra%20Case", "Spectrum%202%20Case", "Clutch%20Case", "Horizon%20Case", "Danger%20Zone%20Case", "Prisma%20Case" };
+        string[] caseNames = new string[] { "CSGO Weapon Case", "Operation Bravo Case", "CSGO weapon case 2", "Winter Offensive Case", " CSGO Weapon Case 3", "Operation Phoenix Case", "Huntsman Case", "Operation Breakout Case", " Operation Vanguard Case", "Chroma Case", "Chroma 2 Case ", " Falchion case", " Shadow Case", " Revolver Case", "Operation Wildfire Case", "Chroma 3 Case", "Gamma Case", "Gamma 2 Case", "Glove Case", "Spectrum Case", "Operation Hydra Case", "Spectrum 2 Case", "Clutch Case", "Horizon Case", "Danger Zone Case", "Prisma Case", "CS20 Case", "Operation Shattered Web Case", "Primsa 2 Case", "Fracture Case", "Operation Broken Fang Case", "Snakebite Case", "Operation Riptide Case", "Dreams and Nightmares Case" };
+        string[] urlExtensionList = new string[] { "CS:GO%20Weapon%20Case", "Operation%20Bravo%20Case", "CS%3AGO%20Weapon%20Case%202", "Winter%20Offensive%20Weapon%20Case", "CS%3AGO%20Weapon%20Case%203", "Operation%20Phoenix%20Weapon%20Case", "Huntsman%20Weapon%20Case", "Operation%20Breakout%20Weapon%20Case", "Operation%20Vanguard%20Weapon%20Case", "Chroma%20Case", "Chroma%202%20Case", "Falchion%20Case", "Shadow%20Case", "Revolver%20Case", "Operation%20Wildfire%20Case", "Chroma%203%20Case", "Gamma%20Case", "Gamma%202%20Case", "Glove%20Case", "Spectrum%20Case", "Operation%20Hydra%20Case", "Spectrum%202%20Case", "Clutch%20Case", "Horizon%20Case", "Danger%20Zone%20Case", "Prisma%20Case", "CS20%20Case", "Shattered%20Web%20Case", "Prisma%202%20Case", "Fracture%20Case", "Operation%20Broken%20Fang%20Case", "Snakebite%20Case", "Operation%20Riptide%20Case", "Dreams%20%26%20Nightmares%20Case" };
         string[] output = new string[urlExtensionList.Length]; //fetchApi(urlExtensionList, currency,client);
 
 
@@ -84,7 +88,7 @@ class Program
 
                 }
                 pass = false;
-                Console.WriteLine("Failed" + urlExtension + " retrying");
+                Console.WriteLine("Failed " + urlExtension);
             }
 
             //my only solution to Error 429
@@ -92,7 +96,7 @@ class Program
             {
                 //20 requests per minute apparently
                 Thread.Sleep(60000);
-                 
+
             }
 
         }
@@ -100,16 +104,16 @@ class Program
 
         return output;
     }
-   
 
-    
+
+
     private static CaseData deSerializeJson(String json)
     {
         //%20 =spacae
         //%3A = colon
-       
-        
-        CaseData cD =  JsonSerializer.Deserialize<CaseData>(json);
+
+
+        CaseData cD = JsonSerializer.Deserialize<CaseData>(json);
 
 
         return cD;

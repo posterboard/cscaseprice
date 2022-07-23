8b523906f0d762b462d76c5d309e8b4956814c03
Brian Huang <zbrianhuang1@gmail.com>
2022-07-22 22:59:38 -0400

    Price Change working
    barely.

--- scrappy_website/cases/query.php
+++ scrappy_website/cases/query.php
@@ -1,3 +1,35 @@
 <?php
+    
+require_once './vendor/autoload.php';//change later
+function queryCaseData($name, $yeslimit, $limit){   
+    $connectionStr = "mongodb+srv://mainuser:UpxzsOcbvTZKsFHO@cluster0.pattjaw.mongodb.net/?retryWrites=true&w=majority";
+    $client = new MongoDB\Client($connectionStr);
+    $db = $client->Case;   
+    $collection = $db->CasePriceData; 
+    if(!$yeslimit){
+        $cursor = $collection->find(['name'=>$name]);
+    }else{
+        //$cursor = $collection->find(['name'=>$name],['limit'=>$limit]);
+        $counter= $collection->count(['name'=>$name]);
+        $iterate = $counter-$limit;
+        $cursor = $collection->find(['name'=>$name],['skip'=>$iterate]);
+        
+    }
+
+    /*
+    foreach ($cursor as $i){
+        echo $i['lowest_price'];
+        echo $i['dateTime'],"\n";
+    }
+    */
+    $arr = array(); 
+    foreach ($cursor as $i){
+        array_push($arr,$i['lowest_price']);
+        
+    }
+    return $arr;
+    
+
+}
 
 ?>
--- scrappy_website/main.php
+++ scrappy_website/main.php
@@ -7,10 +7,12 @@
   <title>CS:GO Case Prices</title>
   <link rel="icon" type="image/x-icon" href="./favicon.ico?">
   <link href="./style.css" rel="stylesheet" type="text/css" />
+  
+  
 </head>
 
 <body>
-  <script src="./script.js"></script>
+ 
   <header>csgo case price viewer.</header>
   <navigation>
     <div class="header">
@@ -18,32 +20,38 @@
       <input type="text" id="searchBar" placeholder="Search..">
     </div>    
   </navigation> 
-  <h1 class="title">Case List:</h1>
- 
+  <h1 id="lol" class="title">Case List:</h1>
+
   <div id="list-of-cases">
+
       <?php 
       require "scrappy_website_db.php";
       require "modify_filename.php";
+      require "./cases/query.php";
+      
       foreach ($case as $i) { 
-        ?>
+        list($pPrice,$cPrice)=queryCaseData($i["name"],1,2);
+      ?>
       <a href=<?php replaceFileName($i["name"])?> > 
-   <!-- <a href ="./case_images/Chroma%2520Case.png"> -->
      
         <div class="case-wrap" data-id="<?=$i["_id"]?>" id="<?=$i["url"]?>">
           <div class="case-name"><?=$i["name"]?></div>
           <div class="case-price">Price: <?=$i["median_price"]?></div>
+          <price past_price= <?php echo $pPrice ?> current_price=<?php echo $cPrice ?>> </price>
           <img src=<?php replaceFileName($i["name"])?>>
         </div>
       </a>
       <?php }
       
-    ?></div>
+    ?>
+    </div>
 
-
-  <footer>
+    <script src="./script.js"></script> 
+    
+    <footer>
 
     
-    <p>Case Prices Last Updated: <?php require "scrappy_website_db.php"; echo $date;?></p>
+    <p>Case Prices Last Updated: <?php require "scrappy_website_db.php"; echo $date;?></p>  
     <a href = "./about.html"><p>About</p></a>
     <a href ="https://github.com/zbrianhuang/cscaseprice"><p>Github</p></a>
   </footer>
